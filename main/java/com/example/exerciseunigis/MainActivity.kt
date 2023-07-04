92b6bf85830ac6591dcca6bbee2d1d57ca1e5a4a
a2r2 <ardroid.attacck@gmail.com>
2023-07-04 16:46:33 -0600

    the images on horizontal recycler view are added

--- main/java/com/example/exerciseunigis/MainActivity.kt
+++ main/java/com/example/exerciseunigis/MainActivity.kt
@@ -27,13 +27,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: MovieAdapter
     private lateinit var adapter2: ItemImageAdapter
 
-
-
-
-
     private lateinit var imageViewExample: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
-
 
         fun loadImageIntoImageView(imageView: ImageView, imageUrl: String) {
             AsyncTask.execute {
@@ -46,7 +41,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
          fun getMovies() {
             val client = OkHttpClient()
             val request = Request.Builder()
@@ -54,9 +48,6 @@ class MainActivity : AppCompatActivity() {
                 .build()
 
             AsyncTask.execute {
-
-
-
                 client.newCall(request).execute().use { response ->
                     val json = JSONObject(response.body()!!.string())
                     val results = json.getJSONArray("results")
@@ -74,51 +65,31 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-
                  AsyncTask.execute {
-
-
                      client.newCall(request).execute().use { response ->
                          val json = JSONObject(response.body()!!.string())
                          val results = json.getJSONArray("results")
-
-                             val movieJson = results.getJSONObject(2)
+                         val movies = mutableListOf<Movie>()
+                         for (i in 0 until results.length()) {
+                             val movieJson = results.getJSONObject(i)
                              val title = movieJson.getString("title")
                              val overview = movieJson.getString("overview")
                              val pick = movieJson.getString("poster_path")
-                         val language = movieJson.getString("original_language")
-                         val dateRealese = movieJson.getString("release_date")
-
-                         val movie = Movie(2, title, overview, pick,language,dateRealese)
-
-                             val images = movie.poster_path
-
-                             val listView2 = findViewById<LinearLayout>(R.id.listViewContent)
-
-                             for (image in images) {
-
-                                 val imageView = ImageView(this)
-
-
-
-                                 // Cargar la imagen en el ImageView (puedes usar una biblioteca como Glide o Picasso para hacerlo)
-                                 loadImageIntoImageView(
-                                     imageView,
-                                     "https://image.tmdb.org/t/p/w500/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg"
-                                 )
-                                 runOnUiThread {
-                                     listView2.addView(imageView)
-                                 }
-
+                             val language = movieJson.getString("original_language")
+                             val dateRealese = movieJson.getString("release_date")
+                             val movie = Movie(i, title, overview, pick, language, dateRealese)
+                             movies.add(movie)
+                         }
+                         val listView2 = findViewById<LinearLayout>(R.id.listViewContent)
+                         for (movie in movies) {
+                             val imageView = ImageView(this)
+                             loadImageIntoImageView(imageView, "https://image.tmdb.org/t/p/w500/${movie.poster_path}")
+                             runOnUiThread {
+                                 listView2.addView(imageView)
                              }
-
+                         }
                      }
-
              }
-
-
-
-
         }
 
 
@@ -127,21 +98,11 @@ class MainActivity : AppCompatActivity() {
 
 
        listView = findViewById(R.id.listView)
-//        listView2 = findViewById(R.id.listView2)
-        val linearLayout = findViewById<LinearLayout>(R.id.listViewContent)
         listView.dividerHeight = 9
         adapter = MovieAdapter(this, ArrayList())
-//        adapter2 = ItemImageAdapter(this, ArrayList())
         listView.adapter = adapter
-//        listView2.adapter = adapter2
-
-
-
-
-
 
       getMovies()
-
 
         val apiKey = "55957fcf3ba81b137f8fc01ac5a31fb5"
         val call = ApiClient.tmdbService.getNowPlaying(apiKey, "en-US", 1)
@@ -156,28 +117,9 @@ class MainActivity : AppCompatActivity() {
             }
 
 
-
-
-
-
-
-
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//
-//            val apiKey = "55957fcf3ba81b137f8fc01ac5a31fb5"
-//            val call = ApiClient.tmdbService.getNowPlaying(apiKey, "en-US", 1)
-//            call.enqueue(object : Callback<MoviesResponse> {
-//                override fun onResponse(call: Call<MoviesResponse>, response: Response<MoviesResponse>) {
-//                    val movies = response.body()?.results ?: emptyList()
-//                    recyclerView.adapter = MoviesAdapter(movies)
-//                }
-
-
-
             override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
                 // Handle error
             }
-
 
         })
     }
